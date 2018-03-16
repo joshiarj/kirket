1aeea38bac1a588ca374d918412a190289bdea81
Asmit Joshi <joshiwin8@hotmail.com>
2018-03-16 08:50:02 +0545

    Slight modification for Python3.6

--- datapull.py
+++ datapull.py
@@ -10,8 +10,8 @@ TODO:
     
 """
 
-#Currently trying it out with Joe Root's debut match. Can extend it to the 
-#full list once we have the wrapper up and running.
+# Currently trying it out with Joe Root's debut match. Can extend it to the
+# full list once we have the wrapper up and running.
 
 import requests
 from bs4 import BeautifulSoup
@@ -22,21 +22,25 @@ import re
 # ABDV = 44936
 # Kane Williamson= 277906
 # Steven Smith = 267192
-playerid = "303669"
-##First let us find all the games a player has played.
+# playerid = "303669"
+# First let us find all the games a player has played.
+
 
 def find_player_matches(playerid):
     """
     Finds the list of ODI innings the batsman has played, 
     and finds the match numbers and returns it as a list.
     """
-    page = requests.get("http://stats.espncricinfo.com/ci/engine/player/"+playerid+".html?class=2;template=results;type=batting;view=innings")
-    soup = BeautifulSoup(page.text)
+    page = requests.get("http://stats.espncricinfo.com/ci/engine/player/{}.html"
+                        "?class=2;template=results;type=batting;view=innings".format(playerid))
+    # soup = BeautifulSoup(page.text)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    # soup = BeautifulSoup(page.text, 'html.parser', markup_type='lxml')
     
     player_match_list = soup.find_all(name='a', attrs={"title":"view the scorecard for this row"})
     
-    #From the above list we need to parse out the match numbers which are part of 
-    #the link. part after the /match/ and before the .html
+    # From the above list we need to parse out the match numbers which are part of
+    # the link. part after the /match/ and before the .html
     
     match_list = []
     for each in player_match_list:
@@ -45,4 +49,3 @@ def find_player_matches(playerid):
         b = b.split(".")       
         match_list.append(b[0])
     return match_list
-    
